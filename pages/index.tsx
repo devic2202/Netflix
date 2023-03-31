a65208103cfa44bcffb899aae596644f205c568a
Devic <devic2202@gmail.com>
2023-03-31 16:22:34 +0700

    update code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import { getSession } from "next-auth/react";
 import { NextPageContext } from "next";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMoviesList";
+import useFavorites from "@/hooks/useFavorites";
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
   if (!session) {
@@ -18,10 +21,14 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 export default function Home() {
+  const {data: movies = []} = useMovieList()
+  const {data: favorites = []} = useFavorites()
   return (
     <>
       <Navbar />
       <Billboard />
+      <MovieList title="Trending Now" data={movies}/>
+      <MovieList title="My List" data={favorites}/>
     </>
   );
 }
